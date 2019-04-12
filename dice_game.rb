91ef337258d9bf74435d8c9dc234f6591f56b121
Laurence Taylor <ltaylorapp@gmail.com>
2019-04-12 10:27:10 +0000

    Updated dice_game.rb

--- dice_game.rb
+++ dice_game.rb
@@ -1,35 +1,35 @@
 def dice_game
-    roll = lambda { rand(6) + 1 }
+  roll = lambda { rand(6) + 1 }
     
-    p1_score = 0
-    p2_score = 0
+  p1_score = 0
+  p2_score = 0
     
+  loop do
     p1_roll = roll.call + roll.call
     p2_roll = roll.call + roll.call
     
-    loop do
-        if p1_roll > p2_roll
-            puts "You rolled #{p1_roll}. Player 2 rolled #{p2_roll}."
-            p1_score += 1
-            puts "Your score is #{p1_score}. Player 2 score is #{p2_score}"
-        elsif p1_roll == p2_roll
-            puts "You rolled #{p1_roll}. Player 2 rolled #{p2_roll}."
-            puts "Your score is #{p1_score}. Player 2 score is #{p2_score}"
-            puts "Tie!"
-        else
-            puts "You rolled #{p1_roll}. Player 2 rolled #{p2_roll}."
-            p2_score += 1
-            puts "Your score is #{p1_score}. Player 2 score is #{p2_score}"
-        end
-        
-        if p1_score >= 2
-            puts "You win!"
-            return true
-        elsif p2_score >= 2
-            puts "You lose!"
-            return false
-        end
+    if p1_roll > p2_roll
+      puts "You rolled #{p1_roll}. Player 2 rolled #{p2_roll}."
+      p1_score += 1
+      puts "Your score is #{p1_score}. Player 2 score is #{p2_score}"
+    elsif p1_roll == p2_roll
+      puts "You rolled #{p1_roll}. Player 2 rolled #{p2_roll}."
+      puts "Your score is #{p1_score}. Player 2 score is #{p2_score}"
+      puts "Tie!"
+    else
+      puts "You rolled #{p1_roll}. Player 2 rolled #{p2_roll}."
+      p2_score += 1
+      puts "Your score is #{p1_score}. Player 2 score is #{p2_score}"
     end
+        
+    if p1_score >= 2
+      puts "You win!"
+      return true
+    elsif p2_score >= 2
+      puts "You lose!"
+      return false
+    end
+  end
 end
 
 dice_game
